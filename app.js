a67bab810d18d83cb988c2da041cea3ec9fc81e1
Антон <tohanknv@yandex.ru>
2022-01-01 22:08:14 +0300

    port fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const mongo = config.get('mongo');
 
 app.use(express.json({extended: true}));
